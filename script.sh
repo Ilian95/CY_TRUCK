12ccf02843980ea7715a8c9776798d087a5c61ed
Ilian95 <133566317+Ilian95@users.noreply.github.com>
2024-01-03 14:07:25 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#------------------------------------------------------------------------------------------------------------------------------------------------
+# 						VERIFICATION
+#------------------------------------------------------------------------------------------------------------------------------------------------
+
+
 # Vérifier si l'argument -h est présent parmi les paramètres
 if [[ "$*" == *-h* ]]; then
     echo -e "Aide : Le chemin du fichier de données doit être le premier argument et est obligatoire.\nUtilisation : $0 [chemin_du_fichier]"
@@ -57,5 +62,42 @@ if [ ! -e "$dossier2" ]; then
 	echo "Le dossier $dossier2 n'existe pas"
 	mkdir "$dossier2"
 fi
+
+#------------------------------------------------------------------------------------------------------------------------------------------------
+# 						TRAITEMENT D1
+#------------------------------------------------------------------------------------------------------------------------------------------------
+
+# Enregistrez le temps de début
+start_time=$(date +%s.%N)
+
+# Créer un dossier temporaire
+tempresult=$(mktemp -d)
+
+# Vérifier si le fichier existe et est un fichier régulier
+file="$1"
+if [ ! -f "$file" ]; then
+    echo "Erreur : Le fichier $file n'existe pas."
+    exit 1
+fi
+
+# Supprimer les doublons dans les colonnes 1 et 6
+awk -F ';' '!seen[$1,$6]++' "$file" > "$tempresult/unique_data.csv"
+
+# Extraire les noms des conducteurs et le nombre de trajets avec tri et comptage
+awk -F ';' '{if ($6 != "") count[$6]++} END {for (name in count) print count[name], name}' "$tempresult/unique_data.csv" | sort -nrt " " -k1 | head -n 10 > "$tempresult/resultat1.txt"
+
+# Afficher les résultats
+awk '{print $2, $3}' "$tempresult/resultat1.txt"
+
+# Enregistrez le temps de fin
+end_time=$(date +%s.%N)
+
+# Calculez la différence de temps
+execution_time=$(echo "$end_time - $start_time" | bc)
+
+echo "Le script a pris $execution_time secondes pour s'exécuter."
+
+# Supprimer le dossier temporaire
+rm -r "$tempresult"
 
 exit 0
