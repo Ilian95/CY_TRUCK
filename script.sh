11e4d5ac8b022d9fb3684032dcc28f612fd2e89a
Ilian95 <133566317+Ilian95@users.noreply.github.com>
2024-01-05 17:26:26 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,13 +6,13 @@
 
 # Vérifier si l'argument -h est présent parmi les paramètres
 if [[ "$*" == *-h* ]]; then
-    echo -e "Aide : Le chemin du fichier de données doit être le premier argument et est obligatoire.\nUtilisation : $0 [chemin_du_fichier] [traitement 1]"
+    echo -e "Aide : Le chemin du fichier de données doit être le premier argument et est obligatoire.\nUtilisation : $0 [chemin_du_fichier] [traitement1] [traitement2] [traitement3]...\nTraitements :\n	---[D1] : option -d1, affiche les conducteurs avec le plus de trajets;\n	---[D2] : option -d2, affiche les conducteurs avec les plus grandes distances;\n	---[L] : option -l, affiche les 10 trajets les plus longs;\n	---[T] : option -t, affiche les 10 villes les plus traversées;\n	---[S] : option -s, affiche les statistiques sur les étapes (distance min, max et moyenne);"
     exit 0
 fi
 
 #Verifie le nb d'arguments
-if [ "$#" -eq 0 ]; then
-	echo "Erreur: Veuillez spécifier le chemin du fichier de données en paramètre et au moins un traitement"
+if [ "$#" -lt 2 ]; then
+	echo "Erreur: Utilisation : $0 [chemin_du_fichier] [traitement1] [traitement2] [traitement3]..."
 	exit 1
 fi
 
@@ -54,23 +54,31 @@ if [ ! -e "$executable" ]; then
 fi
 
 #Verifie la présence de temp le crée si il n'existe pas et supprime son contenu si il existe
-dossier1="temp"
-contenu=$(ls -A "$dossier1" | wc -l)
-if [ ! -e "$dossier1" ]; then
-	echo "Le dossier $dossier1 n'existe pas"
-	mkdir "$dossier1"
+dossier_temp="temp"
+contenu=$(ls -A "$dossier_temp" | wc -l)
+if [ ! -e "$dossier_temp" ]; then
+	echo "Le dossier $dossier_temp n'existe pas"
+	mkdir "$dossier_temp"
 else
 	if  [ ! "$contenu" -eq 0 ]; then
-		rm -r "$dossier1"/*
+		rm -r "$dossier_temp"/*
 	fi
 fi
 
 #Verifie la présence de images et le crée si il n'existe pas
-dossier2="images"
-if [ ! -e "$dossier2" ]; then
-	echo "Le dossier $dossier2 n'existe pas"
-	mkdir "$dossier2"
+dossier_images="images"
+if [ ! -e "$dossier_images" ]; then
+	echo "Le dossier $dossier_images n'existe pas"
+	mkdir "$dossier_images"
 fi
+
+#Verifie que les traitements sont corrects
+for arg in "${@:2}";do
+	if [[ ! "$arg" == *-d1* ]] && [[ ! "$arg" == *-d2* ]] && [[ ! "$arg" == *-l* ]] && [[ ! "$arg" == *-t* ]] && [[ ! "$arg" == *-s* ]] ; then
+		echo -e "Erreur sur les traitements:\nUtilisation : $0 [chemin_du_fichier] [traitement1] [traitement2] [traitement3]..."
+		exit 5
+	fi
+done
 
 #------------------------------------------------------------------------------------------------------------------------------------------------
 # 						TRAITEMENT D1
@@ -81,17 +89,21 @@ if [[ "$*" == *-d1* ]]; then
 	# Enregistrez le temps de début
 	start_time=$(date +%s.%N)
 
-	# Créer un dossier temporaire
-	tempresult=$(mktemp -d)
+	# Vérifier si le fichier existe et est un fichier régulier
+	file="$1"
+	if [ ! -f "$file" ]; then
+	    echo "Erreur : Le fichier $file n'existe pas."
+	    exit 6
+	fi
 
 	# Supprimer les doublons dans les colonnes 1 et 6
-	awk -F ';' '!seen[$1,$6]++' "$file" > "$tempresult/unique_data.csv"
+	awk -F ';' '!seen[$1,$6]++' "$file" > "$dossier_temp/unique_data.csv"
 
 	# Extraire les noms des conducteurs et le nombre de trajets avec tri et comptage
-	awk -F ';' '{if ($6 != "") count[$6]++} END {for (name in count) print count[name], name}' "$tempresult/unique_data.csv" | sort -nrt " " -k1 | head -n 10 > "$tempresult/resultat1.txt"
+	awk -F ';' '{if ($6 != "") count[$6] ++} END {for (name in count) print (name) ";" count[name]}' "$dossier_temp/unique_data.csv" | sort -nrt ";" -k2 | head -n 10 > "$dossier_temp/resultat_d1.txt"
 
 	# Afficher les résultats
-	awk '{print $2, $3}' "$tempresult/resultat1.txt"
+	awk '{print $1, $2, $3}' "$dossier_temp/resultat_d1.txt"
 
 	# Enregistrez le temps de fin
 	end_time=$(date +%s.%N)
@@ -100,9 +112,20 @@ if [[ "$*" == *-d1* ]]; then
 	execution_time=$(echo "$end_time - $start_time" | bc)
 
 	echo "Le script a pris $execution_time secondes pour s'exécuter."
-
-	# Supprimer le dossier temporaire
-	rm -r "$tempresult"
+	
+	gnuplot -e " 
+	set terminal pngcairo size 800,600; 
+	set output 'images/histogramme_d1.png'; 
+	set title 'Option -d1 : Nb Routes = f(Driver)'; 
+	set xlabel 'NB ROUTES'; 
+	set ylabel 'DRIVERS NAMES';
+	set yrange [0:*];
+	set style fill solid; 
+	unset key; 
+	plot 'temp/resultat_d1.txt' using (3):ytic(1) with boxes lc rgb 'skyblue'; 
+	set output; 
+	"
+		
 fi
 
 exit 0
