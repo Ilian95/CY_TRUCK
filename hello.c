2e2b7763ae08df0242343fce9cd645121a5685c3
Ilian95 <133566317+Ilian95@users.noreply.github.com>
2024-01-28 19:29:30 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -6,155 +6,125 @@
 #define MAX_VILLES 10
 
 // Structure pour stocker les informations sur une ville
-typedef struct avl{
-    char nom[TAILLE_VILLE];
-    int totalVisites;
-    struct avl* fg;
-    struct avl* fd;
-    int equilibre;
-} AVL;
+typedef struct avl {
+  int totalVisites;
+  int Visites_D;
+  char nom[50];
+  struct avl *fd;
+  struct avl *fg;
+  int equilibre;
+} Avl;
 
-// Crée un nouveau nœud de l'AVL
-AVL *creerAVL(char *nom) {
-    AVL* ville = (AVL*)malloc(sizeof(AVL));
-    if(ville == NULL){
-    	exit(1);
-    }
-    strcpy(ville->nom, nom);
-    ville->totalVisites = 1;
-    ville->fg = NULL;
-    ville->fd = NULL;
-    ville->equilibre = 1;
-    return ville;
+// Crée un nouveau nœud pour une ville avec son nom et initialise les données
+Avl *creerAvl(char* n) {
+  Avl *pnew = malloc(sizeof(Avl));
+  if (pnew == NULL) {
+    exit(1);
+  }
+  strcpy(pnew->nom, n);
+  pnew->totalVisites = 1;
+  pnew->Visites_D = 0;
+  pnew->fd = NULL;
+  pnew->fg = NULL;
+  pnew->equilibre = 0;
+  return pnew;
 }
 
-// Retourne le maximum entre deux entiers
+// Fonction utilitaire pour retourner le maximum entre deux entiers
 int max(int a, int b) {
-    return (a > b) ? a : b;
+  if (a >= b) {
+    return a;
+  }
+  return b;
 }
 
-// Retourne le maximum entre 3 entiers
-int max2(int a, int b, int c) {
-    return max(max(a,b), c);
-}
-
-// Retourne le minimum entre deux entiers
+// Fonction utilitaire pour retourner le minimum entre deux entiers
 int min(int a, int b) {
-    return (a > b) ? b : a;
+  if (a >= b) {
+    return b;
+  }
+  return a;
 }
 
-// Retourne le minimum entre 3 entiers
+// Fonction utilitaire pour retourner le minimum entre trois entiers
 int min2(int a, int b, int c) {
-    return min(min(a,b), c);
+  return min(min(a, b), c);
 }
 
-// Effectue rotation Gauche dans l'AVL
-AVL *rotationG(AVL *a) {
-	int eq_pivot;
-	int eq_a;
-	AVL *pivot;
-	pivot = a->fd;
-	a->fd = pivot->fg;
-	pivot->fg = a;
-	eq_a = a->equilibre;
-	eq_pivot = pivot->equilibre;
-	a->equilibre = eq_a - max(eq_pivot, 0) - 1;
-	pivot->equilibre = min2(eq_a - 2, eq_a + eq_pivot - 2, eq_pivot - 1);
-	a = pivot;
-	return a;
+// Fonction utilitaire pour retourner le maximum entre trois entiers
+int max2(int a, int b, int c) {
+  return max(max(a, b), c);
 }
 
-// Effectue rotation Droite dans l'AVL
-AVL *rotationD(AVL *a) {
-	int eq_pivot;
-	int eq_a;
-	AVL *pivot;
-	pivot = a->fg;
-	a->fg = pivot->fd;
-	pivot->fd = a;
-	eq_a = a->equilibre;
-	eq_pivot = pivot->equilibre;
-	a->equilibre = eq_a - min(eq_pivot, 0) + 1;
-	pivot->equilibre = max2(eq_a + 2, eq_a + eq_pivot + 2, eq_pivot + 1);
-	a = pivot;
-	return a;
+// Fonctions pour effectuer les rotations dans l'arbre AVL
+Avl *rotationGauche(Avl *a) {
+  int eq_pivot;
+  int eq_a;
+  Avl *pivot;
+  //Effectue la rotation
+  pivot = a->fd;
+  a->fd = pivot->fg;
+  pivot->fg = a;
+  //Met a jour l'équlibre
+  eq_a = a->equilibre;
+  eq_pivot = pivot->equilibre;
+  a->equilibre = eq_a - max(eq_pivot, 0) - 1;
+  pivot->equilibre = min2(eq_a - 2, eq_a + eq_pivot - 2, eq_pivot - 1);
+  a = pivot;
+  return a;
 }
 
-// Effectue double rotation Gauche dans l'AVL
-AVL *DoubleRG(AVL *a) {
-  a->fd = rotationD(a->fd);
-  return rotationG(a);
+Avl *rotationDroite(Avl *a) {
+  int eq_pivot;
+  int eq_a;
+  Avl *pivot;
+  pivot = a->fg;
+  a->fg = pivot->fd;
+  pivot->fd = a;
+  eq_a = a->equilibre;
+  eq_pivot = pivot->equilibre;
+  a->equilibre = eq_a - min(eq_pivot, 0) + 1;
+  pivot->equilibre = max2(eq_a + 2, eq_a + eq_pivot + 2, eq_pivot + 1);
+  a = pivot;
+  return a;
 }
 
-// Effectue double rotation Droite dans l'AVL
-AVL *DoubleRD(AVL *a) {
-  a->fg = rotationG(a->fg);
-  return rotationD(a);
+Avl *DoubleRG(Avl *a) {
+  a->fd = rotationDroite(a->fd);
+  return rotationGauche(a);
 }
 
-// Fonction qui équilibre l'arbre
-AVL *equilibreAVL(AVL *a) {
+Avl *DoubleRD(Avl *a) {
+  a->fg = rotationGauche(a->fg);
+  return rotationDroite(a);
+}
+
+// Fonction pour maj l'équilibre d'un nœud dans l'arbre
+Avl *equilibreAVL(Avl *a) {
   if (a->equilibre >= 2) {
     if (a->fd->equilibre >= 0) {
-      return rotationG(a);
-    } 
-    else {
+      return rotationGauche(a);
+    } else {
       return DoubleRG(a);
     }
-  } 
-  else if (a->equilibre <= -2) {
+  } else if (a->equilibre <= -2) {
     if (a->fg->equilibre <= 0) {
-      return rotationD(a);
-    } 
-    else {
+      return rotationDroite(a);
+    } else {
       return DoubleRD(a);
     }
   }
   return a;
 }
 
-// Fonction pour insérer une ville dans l'arbre
-AVL *insererAVL(AVL *a, char *nom, int *h) {
-	
-	// Insère le nouvel élément au bon endroit
-    if (a == NULL) {
-    	*h = 1;
-        return creerAVL(nom);
-	}
-    if (strcmp(nom, a->nom) < 0){
-        a->fg = insererAVL(a->fg, nom, h);
-        *h=-*h;
-    }
-    else if (strcmp(nom, a->nom) > 0)
-        a->fd = insererAVL(a->fd, nom, h);
-    else {
-    	*h = 0;
-        (a->totalVisites)++;
-        return a;
-    }
-
-    // Mettre à jour la hauteur du nœud
-    if (*h != 0) {
-		a->equilibre = a->equilibre + *h;
-		a = equilibreAVL(a);
-		if (a->equilibre == 0) {
-		  *h = 0;
-		} else {
-		  *h = 1;
-		}
-  	}
-    return a;
-}
-
 // Parcours l'arbre pour récupérer les 10 villes avec le plus de visites
-void parcoursPrefixe(AVL *racine, AVL *topVilles[], int *nbVilles) {
+void parcoursPrefixe(Avl *racine, Avl** topVilles, int *nbVilles) {
     if (racine != NULL) {
         parcoursPrefixe(racine->fg, topVilles, nbVilles);
 
         if (*nbVilles < MAX_VILLES) {
             topVilles[(*nbVilles)++] = racine;
-        } 
-        else {
+        } else {
             int minVisites = topVilles[0]->totalVisites;
             int minIndex = 0;
 
@@ -169,21 +139,85 @@ void parcoursPrefixe(AVL *racine, AVL *topVilles[], int *nbVilles) {
                 topVilles[minIndex] = racine;
             }
         }
+
         parcoursPrefixe(racine->fd, topVilles, nbVilles);
     }
 }
 
-int comparerVisites(const void *a, const void *b) {
-    const AVL *villeA = *(const AVL **)a;
-    const AVL *villeB = *(const AVL **)b;
-    return (villeB->totalVisites - villeA->totalVisites);
+// Insère dans un AVL
+Avl *insererVille(Avl *a, char* nom, int *h) {
+  if (a == NULL) {
+    *h = 1;
+    return creerAvl(nom);
+  } 
+  else if (strcmp(nom, a->nom) < 0) {
+    a->fg = insererVille(a->fg, nom, h);
+    *h = -*h;
+  } 
+  else if (strcmp(nom, a->nom) > 0) {
+    a->fd = insererVille(a->fd, nom, h);
+  } 
+  else {
+    *h = 0;
+    a->totalVisites ++;
+    return a;
+  }
+  if (*h != 0) {
+    a->equilibre = a->equilibre + *h;
+    a = equilibreAVL(a);
+    if (a->equilibre == 0) {
+      *h = 0;
+    } else {
+      *h = 1;
+    }
+  }
+  return a;
+}
+
+// Insère dans un AVL
+Avl *insererVilleD(Avl *a, char* nom, int *h) {
+  if (a == NULL) {
+    *h = 1;
+    return creerAvl(nom);
+  } 
+  else if (strcmp(nom, a->nom) < 0) {
+    a->fg = insererVille(a->fg, nom, h);
+    *h = -*h;
+  } 
+  else if (strcmp(nom, a->nom) > 0) {
+    a->fd = insererVille(a->fd, nom, h);
+  } 
+  else {
+    *h = 0;
+    a->totalVisites ++;
+    a->Visites_D ++;
+    return a;
+  }
+  if (*h != 0) {
+    a->equilibre = a->equilibre + *h;
+    a = equilibreAVL(a);
+    if (a->equilibre == 0) {
+      *h = 0;
+    } else {
+      *h = 1;
+    }
+  }
+  return a;
+}
+
+int comparerVilles(const void *a, const void *b) {
+    const Avl *villeA = *(const Avl **)a;
+    const Avl *villeB = *(const Avl **)b;
+    return strcmp(villeA->nom, villeB->nom);
 }
 
 int main() {
-    FILE *fichier;
-    char ligne[1024];
+	FILE *fichier;
+	Avl *a = NULL;
+	int *h = malloc(sizeof(int));
+	char ligne[1024];
 
-    AVL *racine = NULL;
+    Avl *racine = NULL;
 
     fichier = fopen("data/data.csv", "r");
     if (fichier == NULL) {
@@ -194,11 +228,10 @@ int main() {
     while (fgets(ligne, sizeof(ligne), fichier)) {
         char *token;
         char *colonnes[5];
-        int *h = malloc(sizeof(int));
 
         int colonne = 0;
         token = strtok(ligne, ";");
-        while (token != NULL && colonne < 5){
+        while (token != NULL && colonne < 5) {
             colonnes[colonne++] = token;
             token = strtok(NULL, ";");
         }
@@ -207,7 +240,7 @@ int main() {
             char *ville = colonnes[3];
 
             if (strlen(ville) < TAILLE_VILLE) {
-                racine = insererAVL(racine, ville, h);
+                racine = insererVille(racine, ville, h);
             }
         }
 
@@ -215,24 +248,26 @@ int main() {
             char *villeDepart = colonnes[2];
 
             if (strlen(villeDepart) < TAILLE_VILLE) {
-                racine = insererAVL(racine, villeDepart, h);
+                racine = insererVilleD(racine, villeDepart, h);
             }
         }
     }
 
     fclose(fichier);
 
-    AVL* topVilles[MAX_VILLES];
+    Avl *topVilles[MAX_VILLES];
     int nbVillesTop = 0;
 
     parcoursPrefixe(racine, topVilles, &nbVillesTop);
 
-    qsort(topVilles, nbVillesTop, sizeof(AVL *), comparerVisites);
+    qsort(topVilles, nbVillesTop, sizeof(Avl *), comparerVilles);
 
     printf("Les 10 villes avec le plus de visites :\n");
     for (int i = 0; i < nbVillesTop && i < MAX_VILLES; i++) {
-        printf("%s : Total Visites - %d\n", topVilles[i]->nom, topVilles[i]->totalVisites);
+        printf("%s;%d;%d\n", topVilles[i]->nom, topVilles[i]->totalVisites, topVilles[i]->Visites_D);
     }
+    
+    free(h);
 
     return EXIT_SUCCESS;
 }
