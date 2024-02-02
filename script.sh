66ce2b6f3b6b58fdffb0073899f601086533f1e1
Ilian95 <133566317+Ilian95@users.noreply.github.com>
2024-02-02 14:52:11 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -408,6 +408,37 @@ if [[ "$*" == *-s* ]]; then
 	./'progc/exec' "$fichier" > "$dossier_temp/resultat_s.csv"
 
 	cat "$dossier_temp/resultat_s.csv"
+ 
+ 	#Ecris le contenu du script gnuplot
+	gnuplot_s="
+	#Créée le graphique
+	set terminal pngcairo size 1100,900  
+	set output 'images/graph_s.png'
+	set datafile separator\";\"
+	set title 'Option -s : Distance = f(Route)'
+	set xlabel 'ROUTE ID'
+	set ylabel 'DISTANCE (Km)'
+	set style data line
+	set xtics rotate by -45 autojustify
+	set xrange [*:*]
+	set yrange [0:1000]
+	plot 'temp/resultat_s.csv' using 1:4:xtic(2) with lines lc rgb '#39A77C' title 'Distance average', 'temp/resultat_s.csv' using 1:3 with lines lc rgb '#19DE91' title 'Distance Max/Min', 'temp/resultat_s.csv' using 1:5 with lines lc rgb '#19DE91' notitle, 'temp/resultat_s.csv' using 1:3:5 with filledcurves lc rgb '#19DE91' fs transparent solid 0.5 notitle
+	"
+	# Écris le script Gnuplot dans un fichier temporaire
+	echo "$gnuplot_s" > "$dossier_temp/graph_s.gp"
+
+	# Exécuter le script Gnuplot
+	gnuplot "$dossier_temp/graph_s.gp"
+	
+	#Affiche le Graphique
+	image_chemin="images/graph_s.png"		
+	
+	# Vérifier si xdg-open est disponible
+	if command -v xdg-open &> /dev/null; then
+	    xdg-open "$image_chemin"
+	else
+	    echo "xdg-open n'est pas disponible sur votre système, le graphique ne pourra pas s'afficher."
+	fi
 	
 	# Enregistrez le temps de fin
 	fin=$(date +%s)
